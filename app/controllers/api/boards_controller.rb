ed4c6962de38814577fb959b66c9a00c4659250d
SrdjanCoric <srdjan.coric1984@gmail.com>
2019-10-28 09:58:31 +0100

    add create method

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -3,4 +3,24 @@ class Api::BoardsController < ApplicationController
     @boards = Board.all
     render :index
   end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      render :create, status: :created
+    else
+      @error = @board.errors.full_messages.join(', ')
+      render 'api/shared/error', status: :unprocessable_entity
+    end
+  rescue ActionController::ParameterMissing
+    @error = "Invalid board data provided"
+    render 'api/shared/error', status: :unprocessable_entity
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title)
+  end
 end
