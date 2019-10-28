bf0f0ca09fe466340c420f900a5520fa003eed08
SrdjanCoric <srdjan.coric1984@gmail.com>
2019-10-28 10:50:53 +0100

    add apiclient

---
+++ app/javascript/lib/ApiClient.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import * as routes from "../constants/ApiRoutes";
+
+function logError(errorResponse) {
+  const response = errorResponse.response;
+
+  if (response && response.data && response.data.error) {
+    console.error(`HTTP Error: ${response.data.error}`);
+  } else {
+    console.error("Error: ", errorResponse);
+  }
+}
+
+function unwrapData(response) {
+  return response.data;
+}
+
+axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+axios.defaults.headers.common["Accept"] = "application/json";
+
+const apiClient = {
+  getBoards: function(callback) {
+    return axios
+      .get(routes.BOARDS_INDEX_URL)
+      .then(unwrapData)
+      .then(callback)
+      .catch(logError);
+  }
+};
+
+export default apiClient;
