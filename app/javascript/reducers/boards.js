8278e299cc41153ea5aa4216faf5e9d716c62cbe
SrdjanCoric <srdjan.coric1984@gmail.com>
2019-10-28 11:13:18 +0100

    add create action in reducer

--- app/javascript/reducers/boards.js
+++ app/javascript/reducers/boards.js
@@ -2,6 +2,9 @@ export default function boards(state = [], action) {
   switch (action.type) {
     case "FETCH_BOARDS_SUCCESS":
       return action.boards;
+    case "CREATE_BOARD_SUCCESS":
+      const newBoard = action.board;
+      return state.concat(newBoard);
     default:
       return state;
   }
