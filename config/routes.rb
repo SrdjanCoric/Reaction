7c88774d05f6f1034a6fb6e2ee6c414176275156
SrdjanCoric <srdjan.coric1984@gmail.com>
2019-10-28 09:48:35 +0100

    set initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  namespace :api do
+    resources :boards, only: [:index]
+  end
+
+  get '/ui/all_boards', to: 'ui#all_boards'
+  get '/ui/single_board', to: 'ui#single_board'
+  get '/ui/create_board', to: 'ui#create_board'
+  get '/ui/card', to: 'ui#card'
+  get '/ui/card_editing_description', to: 'ui#card_editing_description'
+  get '/ui/card_archived', to: 'ui#card_archived'
+  get '/ui/due_date_popover', to: 'ui#due_date_popover'
+  get '/ui/labels_popover', to: 'ui#labels_popover'
+  get '/ui/move_card_popover', to: 'ui#move_card_popover'
+  get '/ui/copy_card_popover', to: 'ui#copy_card_popover'
+  get '/ui', to: 'ui#index'
 end
